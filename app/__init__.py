8b55bed48abe9960a1acf609a6bb753e32f1a9af
softMaina <allancodes@gmail.com>
2019-02-19 12:16:28 +0300

    [Chores #164070267] correct db connection errors

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,15 @@ def create_app(config_name):
     from app.api.v2.views import office_view
     from app.api.v2.views import user_view
     from app.api.v2.views import party_view
+    from app.api.v2.views import candidate_view
+    from app.api.v2.views import vote_view
+    from app.api.v2.views import petition_view
 
     app.register_blueprint(party_view.party_route)
     app.register_blueprint(user_view.auth_route)
     app.register_blueprint(office_view.office_route)
+    app.register_blueprint(candidate_view.candidate_route)
+    app.register_blueprint(vote_view.vote_route)
+    app.register_blueprint(petition_view.petition_route)
 
     return app
